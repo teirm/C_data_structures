ed919525d96b8a24a9122f3b827ab918263151e7
teirm <tribunus.vii@gmail.com>
2017-07-04 09:36:10 -0400

    Corrected a memor leak in the insert function found with valgrind.
    Brought code up to standards and renamed tests.

---
+++ linked_list/drivers/linked_list_tests.h
@@ -0,0 +1,22 @@
+/* Author: Cyrus Ramavarapu
+ * Date: Tue 04 Jul 2017 08:01:41 AM EDT
+ * Purpose: Header files for linked list tests
+ */
+
+int  test_length(struct node **list_head);
+
+int  append_node(int new_value, struct node **list_head);
+
+void traverse_list(struct node **list_head);
+
+void test_next(int position, struct node **list_head);
+
+void test_previous(int position, struct node **list_head);
+
+void test_replace(int value, int position, struct node **list_head);
+
+void test_insert(int value, int position, struct node **list_head);
+
+void test_delete(int position, struct node **list_head);
+
+void free_all(struct node **list_head);

81ec7d96ce9857bff9ec365520c03a6a83f0a583
teirm <tribunus.vii@gmail.com>
2017-07-25 22:06:49 -0400

    Cleaned up -- going to do an array implemenation like a sensible person

--- priority_queue/priority_queue.c
+++ priority_queue/priority_queue.c
@@ -16,38 +16,6 @@ struct priority_node *
 delete_min(
         struct priority_queue       *pq)
 {
-    struct priority_node *old_root      = pq->root;        
-    struct priority_node *new_root      = pq->next_delete;
-    struct priority_node *old_parent    = new_root->parent;
-    
-
-    if (old_parent->left_child == new_root) {
-        old_parent->left_child = NULL;
-    } else {
-        old_parent->right_child = NULL;
-    }
-
-    new_root->left_child = old_root->left_child;
-    new_root->right_child = old_root->right_child;
-    
-    old_root->left_child = NULL;
-    old_root->right_child = NULL;
-
-    pq->root = new_root;
-    
-    /* Float new root value down */
-
-    return old_root;
-}
-
-
-struct priority_node *
-right_most_null(
-        struct priority_node        *pn)
-{
-    struct priority_node *next_parent = NULL; 
-    
-
 }
 
 
--- priority_queue/priority_queue.h
+++ priority_queue/priority_queue.h
@@ -14,17 +14,13 @@
 
 struct priority_node {
     int priority;                           /* Priority of the node -- int for convenience */
+    int index;                              /* Index of the node */ 
     void *value;                            /* Value held by the node */ 
     
-    struct priority_node *parent;           /* Point to the parent -- useful for deletions */
-    struct priority_node *left_child;       /* Pointer to the left child */
-    struct priority_node *right_child;      /* Pointer to the right child */
 };
 
 struct priority_queue {
-    struct priority_node *root;             /* Pointer to the root of the tree */
-    struct priority_node *next_delete;      /* Pointer to the next node to use on delete_min */
-    struct priority_node *next_insert;      /* Pointer to the parent where next insertion will occur */
+    struct priority_node **root;             /* Pointer to the root of the tree */
 };
 
 /* Delete and return the node with the highest priority */
@@ -32,12 +28,6 @@ struct priority_node *delete_min(struct priority_queue *pq);
 
 /* Insert a node with some priority into the tree */
 int insert(struct priority_queue *pq, struct priority_node *pn);
-
-/* get the right most leaf from the previous parent */
-struct priority_node *right_most_leaf(struct priority_node *pn);
-
-/* get parent with right most null */
-struct priority_node *right_most_null(struct priority_node *pn); 
 
 /* Swap the priorities and values of two nodes */
 int swap(struct priority_node *a, struct priority_node *b);

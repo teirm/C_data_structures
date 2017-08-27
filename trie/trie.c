97d6234e5e676cd8750feedf56545ae75f6ec5e7
teirm <tribunus.vii@gmail.com>
2017-08-27 19:42:01 -0400

    Reduced functions

--- trie/trie.c
+++ trie/trie.c
@@ -5,5 +5,34 @@
  * Date: 27 August 2017
  */
 
+#include "trie.h"
 
+struct trie_node*
+create_node(
+        struct trie_node            *root,
+        const char                      c)
+{
+    struct trie_node *new_node      = NULL;
 
+    new_node = calloc(1, sizeof *new_node);
+
+    new_node->domain = c;
+    new_node->value = NULL;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+int
+insert(
+        struct trie_node            *root,
+        const char                  *word)
+{
+}
+
+int
+delete_word(
+        struct trie_node            *root,
+        const char                  *word)
+{
+}

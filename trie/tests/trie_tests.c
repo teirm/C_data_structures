279cfc0f6cd3aaf65f320154658b78d6cae05fae
teirm <tribunus.vii@gmail.com>
2017-08-27 19:50:02 -0400

    Started test suite to check (out of paranoia) something regarding const char *

---
+++ trie/tests/trie_tests.c
@@ -0,0 +1,18 @@
+/* 
+ * Purpose: Test suite for the trie data structure
+ *
+ * Author: Cyrus ramavarapu
+ * Date: 27 August 2017
+ */
+
+#include <stdio.h>
+
+/* Main entry point */
+int main()
+{
+    const char *a = "dogs";
+    
+    printf("a[0] = %c\n", a[0]);
+
+    return 0;
+}

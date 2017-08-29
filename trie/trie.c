ad11f881008d105e056ae98618181be84fd47f4b
teirm <tribunus.vii@gmail.com>
2017-08-28 20:53:53 -0400

    Added debugging and beginnings of delete word

--- trie/trie.c
+++ trie/trie.c
@@ -8,6 +8,12 @@
 #include "trie.h"
 #include <ds/debug.h>
 
+#define DEBUG 0
+
+#if DEBUG > 0 
+#include <stdio.h>
+#endif /* DEBUG */
+
 
 struct trie_node*
 create_node(
@@ -36,19 +42,38 @@ insert(
     struct trie_node *new_node      = NULL; 
 
     while (current_char != '\0') {
-        
         if (current_node->domain == current_char) {
             current_char = word[pos++];
+#if DEBUG > 0 
+            printf("[%s:%d] word: %s pos %d current_char: %c\n",
+                    DEBUG_INFO, word, pos, current_char);
+#endif /* DEBUG */
             if (current_node->value) {
-                current_node = current_node->value; 
+                current_node = current_node->value;
+#if DEBUG > 0 
+            printf("[%s:%d] match prefix: current_node: %p current_node->domain: %c\n",
+                    DEBUG_INFO, current_node, current_node->domain);
+#endif /* DEBUG */
             } else {
                 new_node = create_node(current_char);
                 current_node->next = new_node;
+#if DEBUG > 0 
+            printf("[%s:%d] match branch: current_node: %p current_node->domain: %c\n",
+                    DEBUG_INFO, current_node, current_node->domain);
+#endif /* DEBUG */
             }
         } else {
             if (current_node->next) {
+#if DEBUG > 0 
+            printf("[%s:%d] mis-match next: current_node: %p current_node->domain: %c\n",
+                    DEBUG_INFO, current_node, current_node->domain);
+#endif /* DEBUG */
                 current_node = current_node->next;
             } else {
+#if DEBUG > 0 
+            printf("[%s:%d] mis-match branch: current_node: %p current_node->domain: %c\n",
+                    DEBUG_INFO, current_node, current_node->domain);
+#endif /* DEBUG */
                 current_node->next = create_node(current_char);
             }
         }
@@ -62,4 +87,22 @@ delete_word(
         struct trie_node            *root,
         const char                  *word)
 {
+    int pos                             = 0; 
+    char current_char                   = word[pos]; 
+    struct trie_node *current_node      = root;
+
+    while (current_char != '\0') {
+        if (current_node->domain == current_char) {
+            /* continue down prefix path */
+        } else {
+            if ((current_node = current_node->next) == NULL) {
+                return 1;
+#if DEBUG > 0 
+                printf("[%s:%d] Word %s not in trie\n",
+                        DEBUG_INFO, word);
+#endif /* DEBUG */
+            }
+        }
+    } 
+    return 0;
 }

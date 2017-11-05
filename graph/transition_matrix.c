fda7d6159f744f8068c55e66bd3bd509ed9a6cef
teirm <tribunus.vii@gmail.com>
2017-11-05 11:20:34 -0500

    Updated function names and documentation for transition matrix

--- graph/transition_matrix.c
+++ graph/transition_matrix.c
@@ -4,10 +4,24 @@
  * Date:    15 October 2017
  */
 
+#include <ds/debug.h>
+
 #include "transition_matrix.h" 
 
+/* graph_trans_matrix_first
+ *
+ * Returns the first index in the neighborhood of
+ * vertex v.
+ *
+ * @param t_mat             - pointer to transition matrix 
+ *                            struct
+ * @param v                 - vertex to search neighborhood
+ *
+ * @return unsigned long    - index of first node in neighbor
+ *                            hood of v
+ */
 unsigned long
-first (
+graph_trans_matrix_first (
     t_matrix        *t_mat,
     graph_node      *v)
 {
@@ -22,18 +36,30 @@ first (
             break;
         }
     }
-    
     return first_node_index;
 }
 
+/* graph_trans_matrix_next
+ *
+ * Return the index after requested index
+ * in neighborhood of v
+ *
+ * @param   t_mat           - pointer to transitiom matrix
+ *                            structure
+ * @param   v               - vertex to search neighborhood of
+ * @param   req_index       - index requested.
+ *
+ * @return unsigned long    - index in neighborhood after requested
+ *                            or -1 if does not exist.
+ */
 unsigned long
-next (
+graph_trans_matrix_next (
     t_matrix        *t_mat,
     graph_node      *v,
     unsigned long   req_index)
 {
     unsigned long   i;
-    unsigned long   next_node_index = 0;
+    unsigned long   next_node_index = -1;
     unsigned long   v_index = v->index;
 
     for (i = 0; i < t_mat->vertices; i++) {
@@ -46,12 +72,25 @@ next (
             req_index--;
         }
     }
-
     return next_node_index;
 }
 
+/* graph_trans_matrix_vertex
+ *  
+ * Return the graph node indexed at req_index
+ * in neighborhood of v if it exists.
+ *
+ * @param   t_mat           - pointer to transitiom matrix
+ *                            structure
+ * @param   v               - vertex to search neighborhood of
+ * @param   req_index       - index requested.
+ *
+ * @return  graph_node *    - pointer to graph node at req_index in
+ *                            neighboord of v if it exists.  Otherwise
+ *                            NULL.
+ */
 graph_node *
-vertex (
+graph_trans_matrix_vertex (
     t_matrix            *t_mat,
     graph_node          *v,
     unsigned long       req_index)
@@ -69,6 +108,5 @@ vertex (
             }
         }
     }
-
     return ret_node;
 }
--- graph/transition_matrix.h
+++ graph/transition_matrix.h
@@ -7,20 +7,29 @@
 
 #include <stdlib.h>
 
-
 typedef struct graph_node {
     unsigned long       index;
     void                *graph_info;
 } graph_node;
-
 
 typedef struct transition_matrix {
     unsigned long       vertices;       
     graph_node          ***matrix;
 } t_matrix;
 
+unsigned long
+graph_trans_matrix_first(
+    t_matrix *t_mat,
+    graph_node *v);
 
+unsigned long
+graph_trans_matrix_next(
+    t_matrix *t_mat,
+    graph_node *v,
+    unsigned long req_index);
 
-unsigned long first(t_matrix *t_mat, graph_node *v);
-unsigned long next(t_matrix *t_mat, graph_node *v, unsigned long req_index);
-graph_node *vertex(t_matrix *t_mat, graph_node *v, unsigned long req_index);
+graph_node *
+graph_trans_matrix_vertex(
+    t_matrix *t_mat,
+    graph_node *v,
+    unsigned long req_index);
